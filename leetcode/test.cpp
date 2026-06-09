65f584e7c24a41484c3d7f60055d62f204a66f28
man <3199347595@qq.com>
2026-06-09 19:12:23 +0800

    前缀乘

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -1,3 +1,4 @@
+//724 题 寻找数组的中心索引
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
@@ -17,5 +18,29 @@ public:
                 return j;
 
         return -1;
+    }
+};
+//238题 除自身以外数组的乘法
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> f(n), g(n);
+        f[0] = 1;
+        g[n - 1] = 1;
+        for (int i = 1;i < n;i++)
+        {
+            f[i] = f[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 2;i >= 0;i--)
+        {
+            g[i] = g[i + 1] * nums[i + 1];
+        }
+        vector<int> ret(n);
+        for (int i = 0; i < n; i++)
+        {
+            ret[i] = g[i] * f[i];
+        }
+        return ret;
     }
 };
