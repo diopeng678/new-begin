c5942faa43803fc159853a3799e7365d3a17d155
man <3199347595@qq.com>
2026-06-12 12:09:43 +0800

    前缀和 + 哈希解决问题

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -78,3 +78,22 @@ public:
         return ret;
     }
 };
+//974 题 和可被 K 整除的子数组 前缀和 + 哈希表
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
+        hash[0 % k] = 1;
+
+        int sum = 0;
+        int ret = 0;
+        for (auto x : nums)
+        {
+            sum += x;     //算出前缀和
+            int r = (sum % k + k) % k;    //修正后的余数
+            if (hash.count(r)) ret += hash[r];
+            hash[r]++;
+        }
+        return ret;
+    }
+};
