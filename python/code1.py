2995721714715585e7e76975298213fff902d305
man <3199347595@qq.com>
2026-08-24 18:10:06 +0800

    python学习

---
+++ python/code1.py
@@ -0,0 +1,30 @@
+print("hello matou");
+# print("hello laobideng");
+#
+# peng = 666;
+# print(peng);
+# print(peng);
+#
+# peng = 6666;
+# print(peng);
+# print(type(peng));
+#
+# man = "ok了兄弟们";
+# print(man);
+# print(type(man));
+
+# print(len(man));
+#
+# print(man + man);
+#
+a = True;
+print(a);
+print(f"a = {a + 10}")
+
+num = input("请输入一个整数:")#返回的是一个字符串类型
+print(f"输入的整数是:{num} ")
+num1 = input("请输入一个整数")
+print(f"输入的整数为:{num1}")
+#验证
+print(f"{num + num1}")#结果是俩个数字连一起
+print(f"{int(num) + int(num1)}")#直接相加需要进行强转
