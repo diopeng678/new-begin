a6ad5254ff807d1fe9580237ad65f4411ab01bfc
man <3199347595@qq.com>
2026-08-31 14:09:32 +0800

    外观数列 双指针+模拟

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -144,3 +144,25 @@ public:
         return ret + duration;
     }
 };
+
+//38 外观数列 
+class Solution {
+public:
+    string countAndSay(int n)
+    {
+        string ret = "1";
+        for (int i = 1;i < n;i++)
+        {
+            string tmp;
+            int len = ret.size();
+            for (int left = 0, right = 0;right < len;)
+            {
+                while (right < len && ret[left] == ret[right]) right++;
+                tmp += to_string(right - left) + ret[left];
+                left = right;
+            }
+            ret = tmp;
+        }
+        return ret;
+    }
+};
